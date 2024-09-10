092e5790583dcb1bdf22321e40a4019cc5c257ce
soltshio <shanshanshien@gmail.com>
2024-09-10 16:49:47 +0900

    スクロール途中経過

--- src/Assets/Scripts/Player_StateAfterPositioning.cs
+++ src/Assets/Scripts/Player_StateAfterPositioning.cs
@@ -16,12 +16,17 @@ public partial class Player
         public override void OnUpdate(Player owner)
         {
             //画面スクロール
-            //画面スクロールし終わったら位置決めに移動
+            owner.cameraScroll.Scroll();
+
+            if(!owner.cameraScroll.ScrollNow)//スクロールしなくなったら位置決めに遷移
+            {
+                owner.ChangeState(statePositioning);
+            }
         }
 
         public override void OnExit(Player owner, PlayerStateBase nextState)
         {
-            owner.putFoot.SwitchFoot();//現在動かしている足を入れ替える
+            owner.currentFoot.SwitchFoot();//現在動かしている足を入れ替える
         }
     }
 }
